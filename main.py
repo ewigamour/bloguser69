17d20b879af4a28fa78b8f85434395b17be1b35c
Han Lin <ewigamour@gmail.com>
2022-11-07 13:50:03 +0100

    Add gunicorn

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ['S_Key']
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['S_Key']
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL", "sqlite:///blog.db"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy()
 db.init_app(app)

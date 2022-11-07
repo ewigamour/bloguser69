6b6dff92b3e0f04a336bad7f222bb35df9a1b69c
Han Lin <ewigamour@gmail.com>
2022-11-07 14:36:44 +0100

    test3

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from functools import wraps
 import os
 import re
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('S_Key')
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = os.environ.get('S_Key')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
